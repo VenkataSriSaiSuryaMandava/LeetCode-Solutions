3d8d809c68f255dc43023b25c5eecb602148f884
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-12 15:31:32 -0500

    Time: 115 ms (55.51%), Space: 61.6 MB (10.38%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,21 +2,17 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         subset = []
-        nums = [i for i in range(1, n + 1)]
 
-        def dfs(i):
+        def backtrack(start):
             if len(subset) == k:
                 res.append(subset.copy())
                 return
 
-            if i == n:
-                return
-                
-            subset.append(nums[i])
-            dfs(i + 1)
-
-            subset.pop()
-            dfs(i + 1)
-        dfs(0)
+            for i in range(start, n + 1):
+                subset.append(i)
+                backtrack(i + 1)
+                subset.pop()
+            
+        backtrack(1)
 
         return res
