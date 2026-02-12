99fa45526ee51c4f694f26eb55457acd5bc2964e
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-12 14:52:02 -0500

    Time: 0 ms (100%), Space: 21.2 MB (11.03%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -14,13 +14,16 @@ class Solution:
         while queue:
             level_len = len(queue)
             level_sum = 0
+
             for i in range(level_len):
                 node = queue.popleft()
                 level_sum += node.val
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
+                    
             res.append(level_sum / level_len)
         return res
                     
