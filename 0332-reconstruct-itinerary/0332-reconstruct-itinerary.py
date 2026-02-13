226d918da50ecc869a3a7e8642c37fbf9fbd81ee
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-13 11:56:58 -0500

    Time: 3 ms (74.67%), Space: 20.1 MB (36.79%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        tickets.sort(reverse=True)
+
+        for src, dest in tickets:
+            adj[src].append(dest)
+        
+        res = []
+
+        def dfs(src):
+            while adj[src]:
+                dst = adj[src].pop()
+                dfs(dst)
+            res.append(src)
+            
+        dfs("JFK")
+        return res[ : : -1]
+            
