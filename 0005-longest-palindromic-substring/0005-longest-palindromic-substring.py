18f8c5f8b933cf3a4a709666694b0aadcd951e89
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-15 13:33:54 -0500

    Time: 291 ms (54.07%), Space: 19.4 MB (32.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,8 +2,14 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
         resLen = 0
+        
+        for i in range(len(s)):
+            resLen, res = self.helper(s, i , i, resLen, res)
+            resLen, res = self.helper(s, i, i + 1, resLen, res)
+        
+        return res
 
-        def helper(l, r, resLen, res):
+    def helper(self, s, l, r, resLen, res):
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if r - l + 1 > resLen:
                     resLen = r - l + 1
@@ -12,9 +18,3 @@ class Solution:
                 r += 1
             
             return (resLen, res)
-        
-        for i in range(len(s)):
-            resLen, res = helper(i , i, resLen, res)
-            resLen, res = helper(i, i + 1, resLen, res)
-        
-        return res
