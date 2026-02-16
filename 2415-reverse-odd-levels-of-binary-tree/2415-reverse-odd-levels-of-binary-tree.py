e7014fab6222b43434220d18f55ecb3e680003cb
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-16 15:44:53 -0500

    Time: 39 ms (18.47%), Space: 23.5 MB (62.21%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -8,21 +8,28 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
+
         queue = deque([root])
         level = 0
+
         while queue:
             if level % 2:
                 l = 0 
                 r = len(queue) - 1
+
                 while l < r:
                     queue[l].val, queue[r].val = queue[r].val, queue[l].val
                     l += 1
                     r -= 1
+
             for i in range(len(queue)):
                 node = queue.popleft()
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
+
             level += 1
+
         return root
