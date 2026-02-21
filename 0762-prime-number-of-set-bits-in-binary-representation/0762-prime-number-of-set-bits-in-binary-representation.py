86844e8dea57c0a1df63dcf3dca6d9378942caa7
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-20 22:16:37 -0500

    Time: 415 ms (26.98%), Space: 19.5 MB (40.29%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -2,12 +2,14 @@ class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
         res = 0
         prime = {2, 3, 5, 7, 11, 13, 17, 19}
-        for n in range(left, right + 1):
+
+        for i in range(left, right + 1):
             count = 0
+            n = i
+
             while n > 0:
-                if n % 2:
-                    count += 1
-                n = n // 2
+                count += n & 1
+                n = n >> 1
 
             if count in prime:
                 res += 1
