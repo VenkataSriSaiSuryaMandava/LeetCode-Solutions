148a51d371c9bc5761b0012c264b44f6ef26a27d
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-22 13:50:07 -0500

    Time: 0 ms (100%), Space: 19.5 MB (21.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,12 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            res = 0
-            while n:
-                digit = n % 10
-                n = n // 10
-                res += digit ** 2
-            n = res
-        return n == 1
+        slow = n
+        fast = self.SumofSquares(n)
+
+        while slow != fast:
+            fast = self.SumofSquares(fast)
+            fast = self.SumofSquares(fast)
+            slow = self.SumofSquares(slow)
+        
+        if fast == 1:
+            return True
+        else:
+            return False
+
+    def SumofSquares(self, n):
+        res = 0
+
+        while n:
+            digit = n % 10
+            res += digit ** 2
+            n = n // 10
+        
+        return res
