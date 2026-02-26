24c9d5171535f87562da8e6bd2cd198f72d6a68c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-02-26 14:15:14 -0500

    Time: 71 ms (32.15%), Space: 25.4 MB (37.09%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,17 +1,37 @@
+class ListNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
 class MyHashSet:
 
     def __init__(self):
-        self.hashSet = set()
+        self.hashSet = [ListNode(0) for i in range(10 ** 4)]
 
     def add(self, key: int) -> None:
-        self.hashSet.add(key)
+        cur = self.hashSet[key % len(self.hashSet)]
+        while cur.next:
+            if cur.next.key == key:
+                return
+            cur = cur.next
+        cur.next = ListNode(key)
 
     def remove(self, key: int) -> None:
-        if key in self.hashSet:
-            self.hashSet.remove(key)
+        cur = self.hashSet[key % len(self.hashSet)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
 
     def contains(self, key: int) -> bool:
-        return key in self.hashSet
+        cur = self.hashSet[key % len(self.hashSet)]
+        while cur.next:
+            if cur.next.key == key:
+                return True
+            cur = cur.next
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
