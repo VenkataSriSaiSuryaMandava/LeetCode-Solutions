daba5655b2748743438579a6ac79cdb36b3fe6fd
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-03 22:19:46 -0500

    Time: 3 ms (82.98%), Space: 20.3 MB (92.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,9 +6,5 @@ class Solution:
         l = 0
         for r in range(len(nums)):
             if nums[r] != 0:
-                nums[l] = nums[r]
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-        
-        while l < len(nums):
-            nums[l] = 0
-            l += 1
