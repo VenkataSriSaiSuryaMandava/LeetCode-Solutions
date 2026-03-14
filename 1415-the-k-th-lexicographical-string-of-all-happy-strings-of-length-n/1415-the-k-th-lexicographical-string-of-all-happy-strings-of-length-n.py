0a4e45114fabd8e4e75e2c15babc5f1cb34492d0
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-14 09:51:08 -0400

    Time: 3 ms (82.52%), Space: 19.2 MB (99.67%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,22 +1,24 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        res = []
-        
-        def dfs(cur):
-            if len(cur) == n:
-                res.append(cur)
-                return
-            
-            if len(res) == k:
-                return 
-            
-            for ch in "abc":
-                if not cur or cur[-1] != ch:
-                    dfs(cur + ch)
-        
-        dfs("")
+        total_happy = 3 * (2 ** (n - 1))
 
-        if len(res) < k:
-            return ""
-        else:
-            return res[k - 1]
+        res = []
+        choices = "abc"
+        left = 1
+        right = total_happy
+
+        for i in range(n):
+            cur = left
+            partition_size = (right - left + 1) // len(choices)
+
+            for c in choices:
+                if cur <= k < cur + partition_size:
+                    res.append(c)
+                    left = cur
+                    right = cur + partition_size - 1
+                    choices = "abc".replace(c, "")
+                    break
+                
+                cur += partition_size
+
+        return "".join(res)
