3b07339e50fb4083e5e74fdc650d4218b6139fc6
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-15 10:21:21 -0400

    Time: 73 ms (20.8%), Space: 20.4 MB (56.54%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,42 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        cur_ans = 0
+        i = 0
+        cur_sign = 1
+        length = len(s)
+
+        while i < len(s):
+            if s[i].isdigit():
+                number = 0
+                digit_index = i
+
+                while digit_index < length and s[digit_index].isdigit():
+                    number = number * 10 + int(s[digit_index])
+                    digit_index += 1
+
+                cur_ans = cur_ans + cur_sign * number
+                i = digit_index - 1
+            
+            elif s[i] == '+':
+                cur_sign = 1
+            
+            elif s[i] == '-':
+                cur_sign = -1
+            
+            elif s[i] == '(':
+                stack.append(cur_ans)
+                stack.append(cur_sign)
+
+                cur_sign = 1
+                cur_ans = 0
+
+            elif s[i] == ')':
+                prev_sign = stack.pop()
+                prev_ans = stack.pop()
+
+                cur_ans = prev_ans + prev_sign * cur_ans
+        
+            i += 1
+
+        return cur_ans
