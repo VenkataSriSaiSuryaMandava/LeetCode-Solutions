c04075199b4035505b473d0694c9a6e64bc766b9
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-17 12:10:49 -0400

    Time: 4 ms (31.69%), Space: 19.6 MB (78.72%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,17 +1,22 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        passChange = [0] * 1001
+        trips.sort(key = lambda t : t[1])
+
+        minHeap = []
+        curPass = 0
 
         for t in trips:
             numPass, start, end = t
-            passChange[start] += numPass
-            passChange[end] -= numPass
-        
-        curPass = 0
-        for i in range(1001):
-            curPass += passChange[i]
+
+            while minHeap and minHeap[0][0] <= start:
+                prevEnd, prevPass = heapq.heappop(minHeap)
+                curPass -= prevPass
             
+            curPass += numPass
+
             if curPass > capacity:
                 return False
+            
+            heapq.heappush(minHeap, [end, numPass])
         
         return True
