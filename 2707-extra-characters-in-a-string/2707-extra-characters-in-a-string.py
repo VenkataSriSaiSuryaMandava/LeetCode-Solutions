5b70b684cc879bab894e31997354441a2b4abf96
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-19 12:21:55 -0400

    Time: 83 ms (63.2%), Space: 25.3 MB (14.13%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+
+        for word in words:
+            cur = self.root
+
+            for c in word:
+                if c not in cur.children:
+                    cur.children[c] = TrieNode()
+                cur = cur.children[c]
+            
+            cur.word = True
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        trie = Trie(dictionary).root
+        dp = {len(s) : 0}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            
+            res = 1 + dfs(i + 1)
+            cur = trie
+
+            for j in range(i, len(s)):
+                if s[j] not in cur.children:
+                    break
+                
+                cur = cur.children[s[j]]
+
+                if cur.word:
+                    res = min(res, dfs(j + 1))
+            
+            dp[i] = res
+            return res
+        
+        return dfs(0)
