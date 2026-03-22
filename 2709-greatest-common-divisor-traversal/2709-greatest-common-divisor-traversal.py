4cb23b2e1b481a362796a144d9e7778ed324552b
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-22 15:45:06 -0400

    Time: 5039 ms (14.76%), Space: 32.9 MB (86.89%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
@@ -0,0 +1,56 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+        self.count = n
+    
+    def find(self, node):
+        while node != self.par[node]:
+            self.par[node] = self.par[self.par[node]]
+            node = self.par[node]
+        
+        return node
+    
+    def union(self, n1, n2):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+
+        if p1 == p2:
+            return 
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        
+        self.count -= 1
+
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        uf = UnionFind(len(nums))
+        factor_index = {}
+
+        for i, n in enumerate(nums):
+            f = 2
+
+            while f * f <= n:
+                if n % f == 0:
+                    if f in factor_index:
+                        uf.union(i, factor_index[f])
+                    else:
+                        factor_index[f] = i
+
+                    while n % f == 0:
+                        n = n // f
+                
+                f += 1
+            
+            if n > 1:
+                if n in factor_index:
+                    uf.union(i, factor_index[n])
+                else:
+                    factor_index[n] = i
+        
+        return uf.count == 1
