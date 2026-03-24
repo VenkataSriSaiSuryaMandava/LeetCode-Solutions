28bbfc27aa428a3d36c761731150474a5641698c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-24 09:40:42 -0400

    Time: 122 ms (58.94%), Space: 42.1 MB (87.42%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        rows = len(grid)
+        cols = len(grid[0])
+        MOD = 12345
+
+        res = [[1] * cols for i in range(rows)]
+
+        prefix = 1
+        for r in range(rows):
+            for c in range(cols):
+                res[r][c] *= prefix
+                prefix = (prefix * grid[r][c]) % MOD
+        
+        suffix = 1
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                res[r][c] = (res[r][c] * suffix) % MOD
+                suffix = (suffix * grid[r][c]) % MOD
+        
+        return res
