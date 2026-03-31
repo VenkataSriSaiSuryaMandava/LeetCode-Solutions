45d81d2a29ab531d5abba62cfbadcdce361719e1
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-03-31 12:50:15 -0400

    Time: 0 ms (100%), Space: 12.3 MB (95.49%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,29 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        mapping = {
-                "2" : "abc", 
-                "3" : "def",
-                "4" : "ghi", 
-                "5" : "jkl", 
-                "6" : "mno", 
-                "7" : "pqrs",
-                "8" : "tuv",
-                "9" : "wxyz"
-                }
-        res = []
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        digitToChar = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz",
+        }
 
+        res = []
+        
         def backtrack(i, cur):
             if len(cur) == len(digits):
                 res.append(cur)
-                return 
-            for c in mapping[digits[i]]:
-                backtrack(i + 1, cur + c)
-        if digits:
-            backtrack(0, "")
+                return
+
+            for ch in digitToChar[digits[i]]:
+                backtrack(i + 1, cur + ch)
+        
+        backtrack(0, "")
         return res
