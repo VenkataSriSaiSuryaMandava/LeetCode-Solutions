c3dd74e3deab9fc14608c9490f66efeba89e8352
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-03 12:51:42 -0400

    Time: 73 ms (41.98%), Space: 12.4 MB (70.35%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -14,7 +14,7 @@ class Solution(object):
         num2 = num2[ : : -1]
 
         for i in range(len(num1)):
-            for j in range(len(num1)):
+            for j in range(len(num2)):
                 digit1 = ord(num1[i]) - ord('0')
                 digit2 = ord(num2[j]) - ord('0')
                 digit = digit1 * digit2
