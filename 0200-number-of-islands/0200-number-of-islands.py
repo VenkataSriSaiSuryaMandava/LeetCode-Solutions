6828726897e08f0254a4856377246e44b48850c2
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-05 11:36:15 -0400

    Time: 285 ms (12.95%), Space: 34.2 MB (5.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -10,7 +10,7 @@ class Solution(object):
         visit = set() 
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        def bfs(r, c):
+        def dfs(r, c):
             if (r < 0 or c < 0 or
                 r == rows or c == cols or
                 (r, c) in visit or
@@ -22,14 +22,14 @@ class Solution(object):
             for dr, dc in directions:
                 row = r + dr
                 col = c + dc
-                bfs(row, col)
+                dfs(row, col)
         
         res = 0
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in visit:
-                    bfs(r, c)
+                    dfs(r, c)
                     res += 1
         
         return res
