a7b74e1b197cb5c335cb6b0cea8955d95b142186
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-05 09:18:35 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,10 +1,14 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(matrix[0]) 
+
         top = 0
         bottom = len(matrix)
-
-        left = 0
-        right = len(matrix[0])
 
         res = []
 
@@ -17,15 +21,15 @@ class Solution:
                 res.append(matrix[i][right - 1])
             right -= 1
 
-            if not (left < right and top < bottom):
+            if not(left < right and top < bottom):
                 break
-
+            
             for i in range(right - 1, left - 1, -1):
                 res.append(matrix[bottom - 1][i])
             bottom -= 1
 
             for i in range(bottom - 1, top - 1, -1):
-                res.append(matrix[i][left])
+                res.append(matrix[left][i])
             left += 1
-  
+        
         return res
