928aa57fe9b38f76769f5338c3d45cba20497127
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-06 09:14:45 -0400

    Time: 6992 ms (20.81%), Space: 12.5 MB (37.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,38 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
         rows = len(board)
         cols = len(board[0])
-        path = set()
+        visit = set()
 
         def backtrack(r, c, i):
             if i == len(word):
                 return True
-            if (r < 0 or c < 0 or 
-                r >= rows or c >= cols or 
-                board[r][c] != word[i] or 
-                (r, c) in path):
+            
+            if (r < 0 or c < 0 or
+                r == rows or c == cols or
+                board[r][c] != word[i] or
+                (r, c) in visit):
                 return False
-            path.add((r, c))
-            res = (backtrack(r + 1, c, i + 1) or 
-                   backtrack(r - 1, c, i + 1) or
+            
+            visit.add((r, c))
+
+            res = (backtrack(r + 1, c, i + 1) or
                    backtrack(r, c + 1, i + 1) or
+                   backtrack(r - 1, c, i + 1) or
                    backtrack(r, c - 1, i + 1))
-            path.remove((r, c))
+            
+            visit.remove((r, c))
+            
             return res
         
         for r in range(rows):
             for c in range(cols):
                 if backtrack(r, c, 0):
                     return True
+        
         return False
