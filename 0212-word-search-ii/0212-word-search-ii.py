7beb471ae7166317b99f6262484c51cfd14e135a
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-06 10:25:20 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -10,30 +10,38 @@ class TrieNode:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
+        
         cur.endOfWord = True
 
-class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+class Solution(object):
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
         root = TrieNode()
-        for w in words:
-            root.addWord(w)
-         
+
+        for word in words:
+            root.addWord(word)
+        
         rows = len(board)
         cols = len(board[0])
 
-        res = set()
         visit = set()
+        res = set()
 
         def backtrack(r, c, node, word):
             if (r < 0 or c < 0 or
-                r >= rows or c >= cols or
+                r == rows or c == cols or
                 (r, c) in visit or
                 board[r][c] not in node.children):
-                return
+                return False
             
-            visit.add((r,c))
+            visit.add((r, c))
             word += board[r][c]
             node = node.children[board[r][c]]
+
             if node.endOfWord:
                 res.add(word)
             
@@ -43,7 +51,7 @@ class Solution:
             backtrack(r, c - 1, node, word)
 
             visit.remove((r, c))
-    
+        
         for r in range(rows):
             for c in range(cols):
                 backtrack(r, c, root, "")
