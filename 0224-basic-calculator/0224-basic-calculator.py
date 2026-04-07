8f014e106e724ca8e9bb7443d82bda6cc7d876d3
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-07 10:13:51 -0400

    Time: 191 ms (5.05%), Space: 13.1 MB (97.16%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,23 +1,29 @@
-class Solution:
-    def calculate(self, s: str) -> int:
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         stack = []
-        cur_ans = 0
-        i = 0
+
         cur_sign = 1
+        cur_ans = 0
+
+        i = 0
         length = len(s)
+        digits = "0123456789"
 
         while i < length:
-            if s[i].isdigit():
+            if s[i] in digits:
                 number = 0
-                digit_index = i
 
-                while digit_index < length and s[digit_index].isdigit():
-                    number = number * 10 + int(s[digit_index])
-                    digit_index += 1
+                while i < length and s[i] in digits:
+                    number = number * 10 + int(s[i])
+                    i += 1
 
+                i -= 1
                 cur_ans = cur_ans + cur_sign * number
-                i = digit_index - 1
-            
+
             elif s[i] == '+':
                 cur_sign = 1
             
@@ -28,15 +34,15 @@ class Solution:
                 stack.append(cur_ans)
                 stack.append(cur_sign)
 
-                cur_sign = 1
                 cur_ans = 0
-
+                cur_sign = 1
+            
             elif s[i] == ')':
                 prev_sign = stack.pop()
                 prev_ans = stack.pop()
 
                 cur_ans = prev_ans + prev_sign * cur_ans
-        
+
             i += 1
 
         return cur_ans
