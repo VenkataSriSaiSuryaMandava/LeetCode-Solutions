8ba0d998ea08f63cdea1f83d4db970211f5ce204
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-09 10:47:17 -0400

    Time: 72 ms (88.98%), Space: 16.2 MB (29.3%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,31 +1,41 @@
-class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
         if endWord not in wordList:
             return 0
         
-        nei = defaultdict(list)
         wordList.append(beginWord)
+        nei = defaultdict(list)
 
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[ : i] + "*" + word[i + 1 : ]
+                pattern = word[ : i] + "*" + word[i + 1: ]
                 nei[pattern].append(word)
-        
+
+        res = 1
         visit = set(beginWord)
         queue = deque([beginWord])
-        res = 1
 
         while queue:
             for i in range(len(queue)):
                 word = queue.popleft()
+
                 if word == endWord:
                     return res
-
+                
                 for j in range(len(word)):
                     pattern = word[ : j] + "*" + word[j + 1 : ]
+
                     for neiWord in nei[pattern]:
                         if neiWord not in visit:
                             visit.add(neiWord)
                             queue.append(neiWord)
+                
             res += 1
+        
         return 0
