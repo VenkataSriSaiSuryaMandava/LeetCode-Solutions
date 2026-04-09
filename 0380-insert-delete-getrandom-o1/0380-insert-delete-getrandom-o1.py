e7c8c383ff21514ea26f793991ce8f8047003853
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-09 10:02:35 -0400

    Time: 217 ms (51.11%), Space: 55.1 MB (76.16%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,46 @@
-class RandomizedSet:
+class RandomizedSet(object):
 
     def __init__(self):
         self.map = {}
-        self.array = []
+        self.arr = []
 
-    def insert(self, val: int) -> bool:
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
         if val in self.map:
             return False
-        else:
-            self.map[val] = len(self.array)
-            self.array.append(val)
-            return True
+        
+        self.map[val] = len(self.arr)
+        self.arr.append(val)
 
-    def remove(self, val: int) -> bool:
+        return True
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
         if val not in self.map:
             return False
-        else:
-            idx = self.map[val]
-            last_value = self.array[-1]
-            self.array[idx] = last_value
-            self.array.pop()
-            self.map[last_value] = idx
-            del self.map[val]
-            return True
+        
+        idx = self.map[val]
+        last = self.arr[-1]
 
-    def getRandom(self) -> int:
-        return random.choice(self.array)
+        self.arr[idx] = last
+        self.arr.pop()
 
+        self.map[last] = idx
+        del self.map[val]
+
+        return True
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.arr)
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
