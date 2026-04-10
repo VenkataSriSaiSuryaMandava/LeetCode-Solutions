1b0825b0f9d3ca3a7e2d221180609090d914d625
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-10 12:00:13 -0400

    Time: 27 ms (33.16%), Space: 18.4 MB (7.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,17 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        groups = defaultdict(list)
+
+        for str in strs:
             count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return list(res.values())
+
+            for ch in str:
+                count[ord(ch) - ord('a')] += 1
+            
+            groups[tuple(count)].append(str)
         
+        return list(groups.values())
