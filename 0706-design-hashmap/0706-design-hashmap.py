aabf2d5a7537796e2ca2c94ab0c5d31c27eeb597
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-10 13:33:30 -0400

    Time: 206 ms (16.01%), Space: 23.5 MB (21.73%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,40 +1,58 @@
 class ListNode:
-
-    def __init__(self, key, val = 0, next = None):
+    def __init__(self, key, val = 0):
         self.key = key
         self.val = val
-        self.next = next
+        self.next = None
 
-class MyHashMap:
+class MyHashMap(object):
 
     def __init__(self):
-        self.hashtable = [ListNode(0) for i in range(10 ** 4)]
-        
-    def put(self, key: int, value: int) -> None:
-        cur = self.hashtable[key % len(self.hashtable)]
+        self.hashmap = [ListNode(0) for i in range(10 ** 4)]
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        cur = self.hashmap[key % len(self.hashmap)]
+
         while cur.next:
             if cur.next.key == key:
                 cur.next.val = value
                 return
+            
             cur = cur.next
-
+        
         cur.next = ListNode(key, value)
 
-    def get(self, key: int) -> int:
-        cur = self.hashtable[key % len(self.hashtable)]
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        cur = self.hashmap[key % len(self.hashmap)]
+
         while cur.next:
             if cur.next.key == key:
                 return cur.next.val
+            
             cur = cur.next
-
+        
         return -1
 
-    def remove(self, key: int) -> None:
-        cur = self.hashtable[key % len(self.hashtable)]
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        cur = self.hashmap[key % len(self.hashmap)]
+
         while cur.next:
             if cur.next.key == key:
                 cur.next = cur.next.next
                 return
+            
             cur = cur.next
 
 
