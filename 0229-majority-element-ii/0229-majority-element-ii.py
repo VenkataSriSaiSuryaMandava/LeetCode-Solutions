3260864069c9caa056cd3950b5831ed735f28862
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-12 11:11:56 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,22 +1,26 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         count = defaultdict(int)
 
-        for n in nums:
-            count[n] += 1
-            
+        for num in nums:
+            count[num] += 1
+
             if len(count) > 2:
                 new_count = defaultdict(int)
 
                 for num, cnt in count.items():
                     if cnt > 1:
                         new_count[num] = cnt - 1
-                
+
                 count = new_count
         
         res = []
-        for n in count:
-            if nums.count(n) > len(nums) // 3:
-                res.append(n)
-
+        for num, cnt in count.items():
+            if cnt > len(nums) // 3:
+                res.append(num)
+        
         return res
