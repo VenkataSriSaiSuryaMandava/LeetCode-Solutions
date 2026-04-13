0970e7eec39792b74d22010624588523065a34cc
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-13 09:42:12 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,25 +1,26 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-
-        l = m + n - 1
+        i = m + n -1
         m -= 1
         n -= 1
+        
         while m >= 0 and n >= 0:
-            if nums1[m] > nums2[n]:
-                nums1[l] = nums1[m]
-                m -= 1
-            else:
-                nums1[l] = nums2[n]
+            if nums1[m] < nums2[n]:
+                nums1[i] = nums2[n]
                 n -= 1
-            l -= 1
-        while m >= 0:
-            nums1[l] = nums1[m]
-            l -= 1
-            m -= 1
+            else:
+                nums1[i] = nums1[m]
+                m -= 1
+            i -= 1
+        
         while n >= 0:
-            nums1[l] = nums2[n]
-            l -= 1
+            nums1[i] = nums2[n]
             n -= 1
+        
