d751958a98bb1db0a9db4ebb589b175fae2c3196
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-13 09:21:31 -0400

    Time: 39 ms (29.75%), Space: 12.5 MB (94.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,28 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def isAlphaNum(ch):
+            return (ord("A") <= ord(ch) <= ord("Z") or
+                    ord("a") <= ord(ch) <= ord("z") or
+                    ord("0") <= ord(ch) <= ord("9"))
+        
+        l = 0
+        r = len(s) - 1
 
-        def isalphanum(ch):
-            return (ord('A') <= ord(ch) <= ord('Z') or
-                    ord('a') <= ord(ch) <= ord('z') or
-                    ord('0') <= ord(ch) <= ord('9'))
-
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            while i < j and not isalphanum(s[i]): 
-                i += 1
-            while i < j and not isalphanum(s[j]):
-                j -= 1
-            if s[i].lower() != s[j].lower():
+        while l < r:
+            while l < r and not isAlphaNum(s[l]):
+                l += 1
+            
+            while l < r and not isAlphaNum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
                 return False
-            i += 1
-            j -= 1
+            
+            l += 1
+            r -= 1
+        
         return True
