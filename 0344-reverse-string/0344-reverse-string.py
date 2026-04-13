e950116450f9f47c50361f32c332247480a3f365
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-13 09:15:02 -0400

    Time: 0 ms (100%), Space: 19.7 MB (82.69%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,9 +1,9 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
+class Solution(object):
+    def reverseString(self, s):
         """
-        Do not return anything, modify s in-place instead.
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
         """
-        
         l = 0
         r = len(s) - 1
 
@@ -11,4 +11,3 @@ class Solution:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
-        
