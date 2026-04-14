a3f95ab524549231a6ffd516dfadc2d271d3e1b7
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-14 09:08:57 -0400

    Time: 109 ms (94.52%), Space: 15.4 MB (63.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,23 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
         l = 0
+        res = 0
         count = defaultdict(int)
         freq = 0
 
         for r in range(len(s)):
             count[s[r]] += 1
             freq = max(freq, count[s[r]])
+
             if (r - l + 1) - freq > k:
                 count[s[l]] -= 1
                 l += 1
+            
             res = max(res, r - l + 1)
+
         return res
