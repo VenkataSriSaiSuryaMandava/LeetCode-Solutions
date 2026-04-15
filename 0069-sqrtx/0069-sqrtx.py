dee38f529c2ac67301cd3e13346697a940956d8c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-15 11:37:05 -0400

    Time: 6 ms (29.34%), Space: 12.5 MB (27.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,16 +1,19 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
         l = 1
         r = x
 
         while l <= r:
             m = (l + r) // 2
-            
-            if m * m < x:
-                l = m + 1
-            elif m * m > x:
+
+            if x < m * m:
                 r = m - 1
+            elif x > m * m:
+                l = m + 1
             else:
                 return m
-                
         return r
