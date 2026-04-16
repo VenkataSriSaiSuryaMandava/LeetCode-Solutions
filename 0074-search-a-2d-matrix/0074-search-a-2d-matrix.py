e8a3401a1767dad0011de8921a7a14b1bcf60214
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-16 09:08:55 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,31 +1,37 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
-
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
         top = 0
-        bottom = rows - 1
+        bottom = len(matrix) - 1
 
         while top <= bottom:
             row = (top + bottom) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
+
+            if target < matrix[row][0]:
                 bottom = row - 1
+            elif target > matrix[row][-1]:
+                top = row + 1
             else:
                 break
+        
         if top > bottom:
             return False
         
-        l = 0
-        r = cols - 1
+        left = 0
+        right = len(matrix) - 1
 
-        while l <= r:
-            m = (l + r) // 2
-            if matrix[row][m] < target:
-                l = m + 1
-            elif matrix[row][m] > target:
-                r = m - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if target > matrix[row][mid]:
+                left = mid + 1
+            elif target < matrix[row][mid]:
+                right = mid - 1
             else:
                 return True
+        
         return False
