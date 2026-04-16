5518036634ee6fc99ce19873a70fbc62ed39aa59
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-16 09:23:51 -0400

    Time: 151 ms (93.43%), Space: 13.4 MB (82.09%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,16 +1,23 @@
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
         l = 1
         r = max(piles)
         res = max(piles)
 
         while l <= r:
             m = (l + r) // 2
+
             count = 0
             for p in piles:
-                count += math.ceil(p / m)
+                count += ((p + m - 1) // m)
+            
             if count <= h:
-                res = min(m, res)
+                res = min(res, m)
                 r = m - 1
             else:
                 l = m + 1
