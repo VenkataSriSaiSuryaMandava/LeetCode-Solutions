9027b6d16c2cb4ecc891f2c0074679f53bbdcd98
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-16 10:17:06 -0400

    Time: 7 ms (24.42%), Space: 12.7 MB (24.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,5 +1,10 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
         A = nums1
         B = nums2
 
@@ -7,7 +12,7 @@ class Solution:
         half = total // 2
 
         if len(B) < len(A):
-            B, A = A, B
+            A, B = B, A
         
         l = 0
         r = len(A) - 1
@@ -16,17 +21,17 @@ class Solution:
             i = (l + r) // 2
             j = half - i - 2
 
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+            Aleft = A[i] if i >= 0 else float("-inf")
+            Aright = A[i + 1] if i + 1 < len(A) else float("inf")
+            Bleft = B[j] if j >= 0 else float("-inf")
+            Bright = B[j + 1] if j + 1 < len(B) else float("inf")
 
             if Aleft <= Bright and Bleft <= Aright:
                 if total % 2:
-                    return min(Aright, Bright)
+                   return min(Aright, Bright)
                 else:
-                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                    return(float(max(Aleft, Bleft) + min(Aright, Bright)) / 2)
             elif Aleft > Bright:
                 r = i - 1
             else:
-                l = i + 1
+                l = i + 1 
