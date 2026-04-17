14ea836cde2aa17b8b45fbc9e9d245dfaacb9ff8
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-17 12:26:35 -0400

    Time: 3 ms (86.6%), Space: 20.4 MB (10.95%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,39 +1,30 @@
-class Node:
+class ListNode:
+
     def __init__(self, val, prev, next):
         self.val = val
-        self.next = next
         self.prev = prev
+        self.next = next
 
-class MyCircularQueue(object):
+class MyCircularQueue:
 
-    def __init__(self, k):
-        """
-        :type k: int
-        """
+    def __init__(self, k: int):
         self.space = k
-        self.left = Node(0, None, None)
-        self.right = Node(0, self.left, None)
+        self.left = ListNode(0, None, None)
+        self.right = ListNode(0, self.left, None)
         self.left.next = self.right
 
-    def enQueue(self, value):
-        """
-        :type value: int
-        :rtype: bool
-        """
+    def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
         
-        cur = Node(value, self.right.prev, self.right)
+        cur = ListNode(value, self.right.prev, self.right)
         self.right.prev.next = cur
         self.right.prev = cur
         self.space -= 1
 
         return True
         
-    def deQueue(self):
-        """
-        :rtype: bool
-        """
+    def deQueue(self) -> bool:
         if self.isEmpty():
             return False
         
@@ -43,34 +34,22 @@ class MyCircularQueue(object):
 
         return True
 
-    def Front(self):
-        """
-        :rtype: int
-        """
+    def Front(self) -> int:
         if self.isEmpty():
             return -1
         
         return self.left.next.val
 
-    def Rear(self):
-        """
-        :rtype: int
-        """
+    def Rear(self) -> int:
         if self.isEmpty():
             return -1
         
         return self.right.prev.val
 
-    def isEmpty(self):
-        """
-        :rtype: bool
-        """
+    def isEmpty(self) -> bool:
         return self.left.next == self.right
 
-    def isFull(self):
-        """
-        :rtype: bool
-        """
+    def isFull(self) -> bool:
         return self.space == 0
 
 
