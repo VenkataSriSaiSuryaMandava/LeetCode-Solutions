06d4df9ed8874e5d65b4ab9aa9e4296789f0f7b3
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-20 09:59:08 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,14 +1,21 @@
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        minHeap = []
-        for n in stones:
-            heapq.heappush(minHeap, -1 * n)
-        while len(minHeap) > 1:
-            y = -1 * heapq.heappop(minHeap)
-            x = -1 * heapq.heappop(minHeap)
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        nums = []
+        for stone in stones:
+            heapq.heappush(nums, -1 * stone) 
+
+        while len(nums) > 1:
+            x = -1 * heapq.heappop(nums)
+            y = -1 * heapq.heappop(nums)
+
             if x != y:
-                heapq.heappush(minHeap, -1 * (y - x))
-        if minHeap:
-            return -1 * minHeap[0]
+                heapq.heappush(nums, -1 * abs(y - x))
+        
+        if stones:
+            return -1 * nums[0]
         else:
             return 0
