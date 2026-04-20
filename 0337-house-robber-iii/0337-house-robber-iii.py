0c69653b0f280cb945cedc619b48697ecf0cea3f
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-20 09:26:42 -0400

    Time: 4 ms (77.51%), Space: 16.4 MB (82.54%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -1,11 +1,15 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rob(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         
         def dfs(node):
             if not node:
