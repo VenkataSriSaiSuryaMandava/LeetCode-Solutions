e6446480453fcb9c796b19a20a196e01c723b368
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-20 09:10:27 -0400

    Time: 4 ms (39.93%), Space: 17 MB (59.79%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -18,9 +18,6 @@ class Solution(object):
             if not (left < node.val < right):
                 return False
 
-            leftTree = dfs(node.left, left, node.val)
-            rightTree = dfs(node.right, node.val, right)
-            
-            return leftTree and rightTree
+            return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
 
         return dfs(root, float("-inf"), float("inf"))
