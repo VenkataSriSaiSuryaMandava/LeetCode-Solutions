88dd1e1ae087f62d60a8e185323aed3b70275687
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-21 11:08:27 -0400

    Time: 721 ms (61.8%), Space: 35.4 MB (72.57%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -11,17 +11,17 @@ class MedianFinder(object):
         """
         heapq.heappush(self.small, -1 * num)
 
-        if self.small and self.large and self.large[0] < -1 * self.small[0]:
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-
-        if len(self.small) > len(self.large) + 1:
+        if self.small and self.large and -1 * self.small[0] > self.large[0]:
             val = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, val)
         
-        if len(self.large) > len(self.small) + 1:
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * val)
+        if len(self.small) > 1 + len(self.large):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.large) > 1 + len(self.small):
+            val = -1 * heapq.heappop(self.large)
+            heapq.heappush(self.small, val)
 
     def findMedian(self):
         """
@@ -29,7 +29,7 @@ class MedianFinder(object):
         """
         if len(self.small) > len(self.large):
             return -1 * self.small[0]
-        elif len(self.small) < len(self.large):
+        elif len(self.large) > len(self.small):
             return self.large[0]
         else:
             return (-1 * self.small[0] + self.large[0]) / 2.0
