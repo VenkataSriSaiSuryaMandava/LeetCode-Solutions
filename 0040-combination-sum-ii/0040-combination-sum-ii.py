49f36007c401efe2ed907db4c6d7f0d548379e0d
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-22 09:21:34 -0400

    Time: 65 ms (5.34%), Space: 12.6 MB (13.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,23 +1,31 @@
-class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
         res = []
-
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+        subset = []
+        candidates.sort()
+        
+        def backtrack(i, curSum):
+            if curSum == target:
+                res.append(subset[ : :])
                 return
-            if i >= len(candidates) or total > target:
+            
+            if i == len(candidates) or curSum > target:
                 return
-
-            cur.append(candidates[i])
-            dfs(i + 1, cur, total + candidates[i])
+            
+            subset.append(candidates[i])
+            backtrack(i + 1, curSum + candidates[i])
 
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1
-                
-            cur.pop()
-            dfs(i + 1, cur, total)
+            
+            subset.pop()
+            backtrack(i + 1, curSum)
+        
+        backtrack(0, 0)
 
-        dfs(0, [], 0)
         return res
