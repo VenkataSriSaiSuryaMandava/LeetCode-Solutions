80ebc1c98a07915520270648c9edab3809a5c189
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-22 09:56:32 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -7,7 +7,9 @@ class Solution(object):
         """
         rows = len(board)
         cols = len(board[0])
+
         visit = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def backtrack(r, c, i):
             if i == len(word):
@@ -15,24 +17,24 @@ class Solution(object):
             
             if (r < 0 or c < 0 or
                 r == rows or c == cols or
-                board[r][c] != word[i] or
-                (r, c) in visit):
+                (r, c) in visit or
+                word[i] != board[r][c]):
                 return False
             
             visit.add((r, c))
+            
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
 
-            res = (backtrack(r + 1, c, i + 1) or
-                   backtrack(r, c + 1, i + 1) or
-                   backtrack(r - 1, c, i + 1) or
-                   backtrack(r, c - 1, i + 1))
+                if backtrack(row, col, i + 1):
+                    return True
             
-            visit.remove((r, c))
-            
-            return res
-        
+            return False
+
         for r in range(rows):
             for c in range(cols):
-                if backtrack(r, c, 0):
+                if (r, c) not in visit and backtrack(r, c, 0):
                     return True
         
         return False
