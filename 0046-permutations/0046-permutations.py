f2caccb36d5528f1131b4b0aba2199ac8bac1f02
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-22 09:33:58 -0400

    Time: 0 ms (100%), Space: 12.6 MB (61.69%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,20 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         perms = [[]]
-        
+
         for n in nums:
-            new_perms = []
+            newPerms = []
+
             for p in perms:
                 for i in range(len(p) + 1):
-                    p_copy = p.copy()
-                    p_copy.insert(i, n)
-                    new_perms.append(p_copy)
-            perms = new_perms
+                    pCopy = p[ : :]
+                    pCopy.insert(i, n)
+                    newPerms.append(pCopy)
+            
+            perms = newPerms
+        
         return perms
