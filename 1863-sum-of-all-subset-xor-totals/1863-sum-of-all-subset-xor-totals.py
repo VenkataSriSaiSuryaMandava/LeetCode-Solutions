419b5bde8c1558fed6fcdccc4c8123a894a3e8e3
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-22 08:59:57 -0400

    Time: 27 ms (23.81%), Space: 12.4 MB (27.47%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,8 +1,19 @@
-class Solution:
-    def subsetXORSum(self, nums: List[int]) -> int:
-        res = 0
+class Solution(object):
+    def subsetXORSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        self.res = 0
 
-        for n in nums:
-            res = res | n
+        def backtrack(i, xorTotal):
+            if i == len(nums):
+                self.res += xorTotal
+                return
+            
+            backtrack(i + 1, xorTotal ^ nums[i])
+            backtrack(i + 1, xorTotal)
         
-        return res * (2 ** (len(nums) - 1))
+        backtrack(0, 0)
+
+        return self.res
