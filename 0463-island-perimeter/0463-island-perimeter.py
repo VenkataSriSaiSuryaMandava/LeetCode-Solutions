17c57e2e07a3572bd2905750522f4ff1c3e22a7a
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-24 13:51:21 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,22 +1,37 @@
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         rows = len(grid)
         cols = len(grid[0])
-        visited = set()
 
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == 0:
+        visit = set()
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or
+                r == rows or c == cols or
+                grid[r][c] == 0):
                 return 1
             
-            if (i, j) in visited:
+            if (r, c) in visit:
                 return 0
+            
+            visit.add((r, c))
+            res = 0
 
-            visited.add((i, j))
-            perimeter = dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
 
-            return perimeter
+                res += dfs(row, col)
+            
+            visit.remove((r, c))
+            return res
         
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]:
-                    return dfs(i, j)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]:
+                    return dfs(r, c)
