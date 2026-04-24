24e2d555bee8c75e06ddf9a5e2d108d3ad7b7695
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-24 14:00:44 -0400

    Time: 0 ms (100%), Space: 12.3 MB (66.48%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,21 +1,26 @@
-class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
-        orderInd = {}
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        indOrder = {}
 
         for i, c in enumerate(order):
-            orderInd[c] = i
+            indOrder[c] = i
         
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
+            w1 = words[i]
+            w2 = words[i + 1]
 
             for j in range(len(w1)):
-                if len(w2) == j:
+                if j == len(w2):
                     return False
                 
                 if w1[j] != w2[j]:
-                    if orderInd[w2[j]] < orderInd[w1[j]]:
+                    if indOrder[w2[j]] < indOrder[w1[j]]:
                         return False
-
                     break
-
-        return True 
+        
+        return True
