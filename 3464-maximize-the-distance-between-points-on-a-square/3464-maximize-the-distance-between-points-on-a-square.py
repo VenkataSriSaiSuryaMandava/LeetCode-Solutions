0d174efb08c71217e76c4718ff02f7d66295cf10
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-25 11:58:37 -0400

    Time: 47 ms (100%), Space: 17.5 MB (100%) - LeetHub

---
+++ 3464-maximize-the-distance-between-points-on-a-square/3464-maximize-the-distance-between-points-on-a-square.py
@@ -0,0 +1,51 @@
+from bisect import bisect_left
+class Solution(object):
+    def maxDistance(self, side, points, k):
+        """
+        :type side: int
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        res = []
+        for x, y in points:
+            if x == 0:
+                res.append(y)
+            elif y == side:
+                res.append(side + x)
+            elif x == side:
+                res.append(side * 3 - y)
+            else:
+                res.append(side * 4 - x)
+        res.sort()
+
+        def check(n):
+            idx = [0] * k
+            curr = res[0]
+            for i in range(1, k):
+                j = bisect_left(res, curr + n)
+                if j == len(res):
+                    return False
+                idx[i] = j
+                curr = res[j]
+            if curr - res[0] <= side * 4 - n:
+                return True
+            
+            for idx[0] in range(1, idx[1]):
+                for j in range(1, k):
+                    while res[idx[j]] < res[idx[j - 1]] + n:
+                        idx[j] += 1
+                        if idx[j] == len(res):
+                            return False
+                if res[idx[-1]] - res[idx[0]] <= side * 4 - n:
+                    return True
+            return False
+        
+        left, right = 1, side + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid
+        return left  
