c759fbe7348c5fa86d02c0bda26dd8a4fb15a3d0
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-26 18:12:28 -0400

    Time: 15 ms (100%), Space: 12.4 MB (100%) - LeetHub

---
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findValidElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = [float("-inf")] * len(nums)
+        leftMax = float("-inf")
+
+        for i in range(len(nums)):
+            left[i] = leftMax
+            leftMax = max(nums[i], leftMax)
+
+        right = [float("-inf")] * len(nums)
+        rightMax = float("-inf")
+        
+        for i in range(len(nums) - 1, -1, -1):
+            right[i] = rightMax
+            rightMax = max(nums[i], rightMax)
+
+        res = []
+
+        for i, n in enumerate(nums):
+            if i == 0 or i == len(nums) - 1:
+                res.append(n)
+
+            elif left[i] < n or right[i] < n:
+                res.append(n)
+
+        return res
