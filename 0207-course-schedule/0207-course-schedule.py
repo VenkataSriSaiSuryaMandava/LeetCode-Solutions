4fb43eb4f83a3f74338cb7179b39cc9e25a1c6c7
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-27 12:11:50 -0400

    Time: 10 ms (45.05%), Space: 16.2 MB (10.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,37 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {i : [] for i in range(numCourses)}
-        for course, pre in prerequisites:
-            preMap[course].append(pre)
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        preMap = defaultdict(list) 
 
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
         visit = set()
 
-        def dfs(course):
-            if course in visit:
+        def dfs(crs):
+            if crs in visit:
                 return False
-            if preMap[course] == []:
+            
+            if not preMap[crs]:
                 return True
             
-            visit.add(course)
-            for pre in preMap[course]:
+            visit.add(crs)
+
+            for pre in preMap[crs]:
                 if not dfs(pre):
                     return False
-            visit.remove(course)
-            preMap[course] = []
+            
+            visit.remove(crs)
+            preMap[crs] = []
+
             return True
-        
-        for course in range(numCourses):
-            if not dfs(course):
+            
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
+        
         return True
