c765d0ba5d1633e8de1848b3b43428381ba7652b
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-27 11:55:28 -0400

    Time: 15 ms (17.34%), Space: 16.6 MB (77.44%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,33 +1,36 @@
-class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+class Solution(object):
+    def solve(self, board):
         """
-        Do not return anything, modify board in-place instead.
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
         """
         rows = len(board)
         cols = len(board[0])
 
-        def capture(r, c):
-            if (r < 0 or r >= rows or
-                c < 0 or c >= cols or
+        directions = [[1, 0], [-1, 0],[0, 1], [0, -1]]
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or 
+                r == rows or c == cols or
                 board[r][c] != "O"):
                 return
+
             board[r][c] = "T"
 
-            capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c - 1)
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
+
+                dfs(row, col)
         
         for r in range(rows):
             for c in range(cols):
-                if (board[r][c] == "O" and 
-                    (r in [0 , rows - 1] or 
-                    c in [0, cols - 1])):
-                    capture(r, c)
+                if board[r][c] == "O" and (r in [0, rows - 1] or c in [0, cols - 1]):
+                    dfs(r, c)
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
+                if board[r][c] == "T":
                     board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
