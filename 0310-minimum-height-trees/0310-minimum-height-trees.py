ddf2bb9279a4808abf80ce7cbe4a03ecd34a38d6
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-28 13:08:46 -0400

    Time: 99 ms (14.59%), Space: 22.8 MB (51.6%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,9 +1,15 @@
-class Solution:
-    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
         if n == 1:
             return [0]
+        
+        adj = defaultdict(list)
 
-        adj = defaultdict(list) 
         for a, b in edges:
             adj[a].append(b)
             adj[b].append(a)
@@ -14,7 +20,7 @@ class Solution:
         for src, nei in adj.items():
             if len(nei) == 1:
                 queue.append(src)
-
+            
             edges_count[src] = len(nei)
         
         while queue:
