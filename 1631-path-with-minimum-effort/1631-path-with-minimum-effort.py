38ef4e86edb029c3fc60d97b31d9cc36fd190a0f
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-04-29 09:03:24 -0400

    Time: 530 ms (25.68%), Space: 14.9 MB (20.85%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,10 +1,15 @@
-class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+class Solution(object):
+    def minimumEffortPath(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: int
+        """
         rows = len(heights)
         cols = len(heights[0])
 
-        minHeap = [[0, 0, 0]]
         visit = set()
+        minHeap = [[0, 0, 0]]
+
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         while minHeap:
@@ -12,19 +17,21 @@ class Solution:
 
             if (r, c) == (rows - 1, cols - 1):
                 return diff
-
+            
             if (r, c) in visit:
                 continue
-            visit.add((r,c))
             
-            for dr, dc in directions:
-                newR = r + dr
-                newC = c + dc
+            visit.add((r, c))
 
-                if (newR < 0 or newC < 0 or 
-                    newR == rows or newC == cols or 
-                    (newR, newC) in visit):
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
+
+                if (row < 0 or col < 0 or
+                    row == rows or col == cols or
+                    (row, col) in visit):
                     continue
                 
-                newDiff = max(diff, abs(heights[newR][newC] - heights[r][c]))
-                heapq.heappush(minHeap, [newDiff, newR, newC])
+                newDiff = max(diff, abs(heights[row][col] - heights[r][c]))
+                heapq.heappush(minHeap, [newDiff, row, col])
+
