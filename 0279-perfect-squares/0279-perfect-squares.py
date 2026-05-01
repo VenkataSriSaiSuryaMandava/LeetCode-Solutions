5a587242f7410ec8b0b95152f30fd175aee5fd27
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-01 12:41:33 -0400

    Time: 3044 ms (63%), Space: 12.6 MB (39.9%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -8,12 +8,8 @@ class Solution(object):
         dp[0] = 0
 
         for target in range(1, n + 1):
-            for s in range(1, target + 1):
+            for s in range(1, int(target ** 0.5) + 1):
                 square = s * s
-
-                if target - square < 0:
-                    break
-                
                 dp[target] = min(dp[target], 1 + dp[target - square])
         
         return dp[n]
