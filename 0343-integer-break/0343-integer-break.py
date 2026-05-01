fe70fd40aa24b9ea0e8318f80e5708ea103e1af5
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-01 12:46:09 -0400

    Time: 3 ms (16.95%), Space: 12.5 MB (19.47%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,15 +1,19 @@
-class Solution:
-    def integerBreak(self, n: int) -> int:
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         dp = {1 : 1}
 
         for num in range(2, n + 1):
-            if num == n:
+            if n == num:
                 dp[num] = 0
             else:
                 dp[num] = num
-            
+
             for i in range(1, num):
                 val = dp[i] * dp[num - i]
                 dp[num] = max(dp[num], val)
-        
-        return dp[n]
+
+        return dp[n]   
