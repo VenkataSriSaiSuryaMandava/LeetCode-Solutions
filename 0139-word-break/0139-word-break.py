3740c5887590a354b64714b98c3bb40b242d9689
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-01 11:11:04 -0400

    Time: 4 ms (78.06%), Space: 12.3 MB (96.44%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -10,7 +10,10 @@ class Solution(object):
 
         for i in range(len(s) - 1, -1, -1):
             for word in wordDict:
-                if word == s[i : i + len(word)]:
+                if i + len(word) <= len(s) and word == s[i : i + len(word)]:
                     dp[i] = dp[i + len(word)]
-        
+
+                if dp[i]:
+                    break
+                    
         return dp[0]
