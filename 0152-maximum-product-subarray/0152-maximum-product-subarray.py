4f1ca288cb2945b6b1693cd33bccc0dc0fcec2f3
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-01 11:05:30 -0400

    Time: 11 ms (59.26%), Space: 12.8 MB (73.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -5,19 +5,19 @@ class Solution(object):
         :rtype: int
         """
         res = max(nums)
-
-        curMin = 1
         curMax = 1
+        curMin = 1
 
         for n in nums:
             if n == 0:
-                curMax = 1 
+                curMax = 1
                 curMin = 1
                 continue
-    
+            
             temp = curMax
             curMax = max(curMax * n, curMin * n, n)
-            curMin = min(curMin * n, temp * n, n)
+            curMin = min(temp * n, curMin * n, n)
+
             res = max(res, curMax)
         
         return res
