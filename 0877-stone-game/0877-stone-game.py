0efbff2566d4557be98766ebe525dbfa8b2b154b
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-04 14:33:46 -0400

    Time: 687 ms (5.23%), Space: 48.1 MB (5.23%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,5 +1,9 @@
-class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
         dp = {}
 
         def dfs(l, r):
