e7ea8e9ec2cfb4fe4b793dd7bd8676b72ba0fce8
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-04 13:25:59 -0400

    Time: 119 ms (45.07%), Space: 12.6 MB (52.2%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,10 @@
-class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def findTargetSumWays(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         dp = defaultdict(int)
         dp[0] = 1
 
@@ -9,6 +14,7 @@ class Solution:
             for cur, count in dp.items():
                 nextDP[cur + nums[i]] += count
                 nextDP[cur - nums[i]] += count
+            
             dp = nextDP
-
-        return dp[target] 
+        
+        return dp[target]
