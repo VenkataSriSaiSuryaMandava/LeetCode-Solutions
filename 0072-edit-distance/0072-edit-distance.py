543db262daa0827bec6801f746522302ca719b93
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-05 09:21:26 -0400

    Time: 117 ms (5.37%), Space: 22.8 MB (8.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,6 +1,11 @@
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dp = [[float("inf") for j in range(len(word2) + 1)] for i in range(len(word1) + 1)]
 
         for i in range(len(word1) + 1):
             dp[i][len(word2)] = len(word1) - i
@@ -13,6 +18,6 @@ class Solution:
                 if word1[i] == word2[j]:
                     dp[i][j] = dp[i + 1][j + 1]
                 else:
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+                    dp[i][j] =  1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
         
         return dp[0][0]
