29e4f7f6300c314e25a9d48b835c887db85cf688
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-05 09:49:13 -0400

    Time: 3 ms (77.36%), Space: 15.3 MB (43.95%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,16 +1,20 @@
-class Solution:
-    def lemonadeChange(self, bills: List[int]) -> bool:
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
         five = 0
         ten = 0
 
-        for b in bills:
-            if b == 5:
+        for bill in bills:
+            if bill == 5:
                 five += 1
-            if b == 10:
+            if bill == 10:
                 ten += 1
             
-            change = b - 5
-
+            change = bill - 5
+            
             if change == 5:
                 if five > 0:
                     five -= 1
@@ -20,8 +24,9 @@ class Solution:
                 if five > 0 and ten > 0:
                     five -= 1
                     ten -= 1
-                elif five >= 3:
+                elif five > 2:
                     five -= 3
                 else:
                     return False
+        
         return True
