20e80fccb086d6168bb0c0ee30a93c2f13ccfb7e
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-19 13:56:32 -0400

    Time: 152 ms (66.03%), Space: 23.9 MB (25.85%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -5,16 +5,17 @@ class Solution:
 
         while queue:
             i = queue.popleft()
+
             start = max(i + minJump, farthest + 1)
             end = min(i + maxJump + 1, len(s))
 
             for j in range(start, end):
-                if s[j] == "0":
+                if s[j] == '0':
                     queue.append(j)
-
+                
                     if j == len(s) - 1:
                         return True
-            
+                
             farthest = i + maxJump
 
         return False
