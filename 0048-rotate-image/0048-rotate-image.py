0a6e061b2958937f365edb62f447e0e40f80d725
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-21 14:22:15 -0400

    Time: 53 ms (0.5%), Space: 19.4 MB (30.3%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        left = 0 
-        right = len(matrix) - 1
+        l = 0
+        r = len(matrix) - 1
 
-        while left < right:
-            top = left
-            bottom = right
+        while l < r:
+            top = l
+            bottom = r
 
-            for i in range(right - left):
-                topleft = matrix[top][left + i]
-                matrix[top][left + i] = matrix[bottom - i][left]
-                matrix[bottom - i][left] = matrix[bottom][right - i]
-                matrix[bottom][right - i] = matrix[top + i][right]
-                matrix[top + i][right] = topleft
+            for i in range(r - l):
+                topLeft = matrix[top][l + i]
+                matrix[top][l + i] = matrix[bottom - i][l]
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+                matrix[bottom][r - i] = matrix[top + i][r]
+                matrix[top + i][r] = topLeft
             
-            left += 1
-            right -= 1
+            l += 1
+            r -= 1
