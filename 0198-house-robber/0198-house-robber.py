71146e0df8d6a1785ecbea6a439a80f7dbe949fc
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-25 11:55:00 -0400

    Time: 0 ms (100%), Space: 19.2 MB (61.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,11 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         rob1 = 0
         rob2 = 0
 
         for n in nums:
-            temp = max(rob2, rob1 + n)
+            temp = rob1
             rob1 = rob2
-            rob2 = temp
+            rob2 = max(rob2, n + temp)
         
         return rob2
