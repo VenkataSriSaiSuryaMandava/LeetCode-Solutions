5255eb507460861a2a78e47eee0e01881d4a8f85
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-05-25 12:39:32 -0400

    Time: 9 ms (64.56%), Space: 20.3 MB (98.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,15 +3,15 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        dp = [float("inf")] * (n + 1) 
+        dp = [float(inf)] * (n + 1)
         dp[-2] = 0
 
         for i in range(m - 1, -1, -1):
-            new_dp = [float("inf")] * (n + 1)
-            
+            newDp = [float(inf)] * (n + 1)
+
             for j in range(n - 1, -1, -1):
-                new_dp[j] = grid[i][j] + min(dp[j], new_dp[j + 1])
-        
-            dp = new_dp
+                newDp[j] = grid[i][j] + min(dp[j], newDp[j + 1])
+            
+            dp = newDp
         
         return dp[0]
