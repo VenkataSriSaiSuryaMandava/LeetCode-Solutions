02869576482cb49cab39302dbf04eed5b2afd8a8
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-02 11:38:14 -0400

    Time: 3 ms (73.4%), Space: 20.8 MB (71.4%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -1,25 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rob(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
             if not node:
                 return [0, 0]
             
-            leftPair = dfs(node.left)
-            rightPair = dfs(node.right)
+            leftpair = dfs(node.left)
+            rightpair = dfs(node.right)
 
-            withRoot = node.val + leftPair[1] + rightPair[1]
-            withoutRoot = max(leftPair) + max(rightPair)
+            withRoot = node.val + leftpair[1] + rightpair[1]
+            withoutRoot = max(leftpair) + max(rightpair)
 
             return [withRoot, withoutRoot]
         
