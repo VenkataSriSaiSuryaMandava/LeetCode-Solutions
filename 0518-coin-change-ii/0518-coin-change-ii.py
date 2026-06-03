8f87040caa8ae8c0ec79bf929834d2f1a238c50d
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-03 12:16:53 -0400

    Time: 371 ms (53.1%), Space: 19.6 MB (54.17%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def change(self, amount, coins):
-        """
-        :type amount: int
-        :type coins: List[int]
-        :rtype: int
-        """
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
 
