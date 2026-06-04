16f1c88b3b88f57200ddabbeed33fa6c7dc82c4c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-04 10:24:10 -0400

    Time: 0 ms (100%), Space: 20.4 MB (95.35%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
 
         while l <= r:
-            m = (l + (r - l) // 2)
+            m = (l + r) // 2
 
             if target > nums[m]:
                 l = m + 1
