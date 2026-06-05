d48e8e7a7e064337af5d0e4ddcf634723448d10d
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-05 13:09:02 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,40 +1,26 @@
-class TimeMap(object):
+class TimeMap:
 
     def __init__(self):
-        self.timestamp = defaultdict(list)
+        self.timestamps = defaultdict(list)
 
-    def set(self, key, value, timestamp):
-        """
-        :type key: str
-        :type value: str
-        :type timestamp: int
-        :rtype: None
-        """
-        self.timestamp[key].append([value, timestamp])
-        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timestamps[key].append([value, timestamp])
 
-    def get(self, key, timestamp):
-        """
-        :type key: str
-        :type timestamp: int
-        :rtype: str
-        """
-        cur = self.timestamp[key]
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.timestamps[key]
 
-        l = 0
-        r = len(cur) - 1
-        res = ""
+        l = 0 
+        r = len(arr) - 1
 
         while l <= r:
             m = (l + r) // 2
 
-            if cur[m][1] <= timestamp:
-                res = cur[m][0]
-                l = m + 1
-            else:
+            if timestamp <= arr[m][1]:
                 r = m - 1
+            else:
+                l = m + 1
         
-        return res
+        return arr[m][0]
 
 
 # Your TimeMap object will be instantiated and called as such:
