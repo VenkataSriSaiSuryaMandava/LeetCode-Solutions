b5043d4310496c2c37ae2eb5a14d8d2dacbf17c8
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-08 15:42:20 -0400

    Time: 0 ms (100%), Space: 19.6 MB (13.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,19 +1,20 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
         top = 0
-        bottom = len(matrix) - 1
+        bottom = rows - 1
+
+        l = 0
+        r = cols - 1
 
         while top <= bottom:
             row = (top + bottom) // 2
 
             if target < matrix[row][0]:
                 bottom = row - 1
-            elif target > matrix[row][-1]:
+            elif target > matrix[row][cols - 1]:
                 top = row + 1
             else:
                 break
@@ -21,16 +22,13 @@ class Solution(object):
         if top > bottom:
             return False
         
-        left = 0
-        right = len(matrix[0]) - 1
+        while l <= r:
+            m = (l + r) // 2
 
-        while left <= right:
-            mid = (left + right) // 2
-
-            if target > matrix[row][mid]:
-                left = mid + 1
-            elif target < matrix[row][mid]:
-                right = mid - 1
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
             else:
                 return True
         
