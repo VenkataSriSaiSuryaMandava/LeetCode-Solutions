9cd9b6a71e460dc4d339da6796d28bcbbfdf84b7
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-08 15:30:29 -0400

    Time: 46 ms (46.32%), Space: 19.3 MB (29.94%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -3,24 +3,19 @@
 # @return -1 if num is higher than the picked number
 #          1 if num is lower than the picked number
 #          otherwise return 0
-# def guess(num):
+# def guess(num: int) -> int:
 
-class Solution(object):
-    def guessNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def guessNumber(self, n: int) -> int:
         l = 1
         r = n
 
         while l <= r:
             m = (l + r) // 2
-            guessNum = guess(m)
 
-            if guessNum == -1:
+            if guess(m) == -1:
                 r = m - 1
-            elif guessNum == 1:
+            elif guess(m) == 1:
                 l = m + 1
             else:
                 return m
