0cdc6b8abcfcdc901caca06c011eab09f7960468
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-16 09:15:19 -0400

    Time: 206 ms (6.16%), Space: 52.7 MB (5.32%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -4,16 +4,15 @@ class Solution:
         res = []
 
         for i, num in enumerate(nums1):
-            heap.append([num + nums2[0], i, 0])
-        heapq.heapify(heap)
-
+            heapq.heappush(heap, [num + nums2[0], i, 0])
+        
         while heap and k > 0:
-            cur_sum, index1, index2 = heapq.heappop(heap)
+            curSum, index1, index2 = heapq.heappop(heap)
             res.append([nums1[index1], nums2[index2]])
             k -= 1
 
             if index2 + 1 < len(nums2):
-                new_sum = nums1[index1] + nums2[index2 + 1]
-                heapq.heappush(heap, [new_sum, index1, index2 + 1])
+                newSum = nums1[index1] + nums2[index2 + 1]
+                heapq.heappush(heap, [newSum, index1, index2 + 1])
         
         return res
