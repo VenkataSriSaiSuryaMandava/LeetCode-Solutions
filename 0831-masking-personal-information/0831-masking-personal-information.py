cef53d0731fee2564f04602f3d1f4a41a6e47d10
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-17 09:15:22 -0400

    Time: 0 ms (100%), Space: 19.4 MB (35.87%) - LeetHub

---
+++ 0831-masking-personal-information/0831-masking-personal-information.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maskPII(self, s: str) -> str:
+        res = ""
+
+        if s[0].isalpha():
+            res += s[0].lower()
+            res += "*****"
+
+            i = 0
+            while s[i + 1] != '@':
+                i += 1
+            res += s[i : ].lower()
+        else:
+            count = 0
+            number = ""
+            for ch in s:
+                if '0' <= ch <= '9':
+                    count += 1
+                    number += ch
+
+            res = '+' if count > 10 else ""
+            localNumber = '***-***-' + number[-4 : ]
+
+            if count > 10:
+                res += '*' * (count - 10) + '-' + localNumber
+                return res
+
+            return localNumber
+        
+        return res
