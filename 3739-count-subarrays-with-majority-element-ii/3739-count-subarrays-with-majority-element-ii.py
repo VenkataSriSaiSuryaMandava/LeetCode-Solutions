3d0dfd9fe8e9323f077ac088152061548e9bedcc
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-26 08:58:06 -0400

    Time: 4353 ms (18.02%), Space: 33 MB (56.76%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        prefix = [0]
+
+        pre = 0
+        for num in nums:
+            pre += 1 if num == target else -1
+            prefix.append(pre)
+
+        res = 0
+        seen = []
+
+        for prefix_sum in prefix:
+            idx = bisect.bisect_left(seen, prefix_sum)
+            res += idx
+            bisect.insort(seen, prefix_sum)
+        
+        return res
