1cd5ddd12231239a79c9c4222e4760764080cc73
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-27 14:57:47 -0400

    Time: 104 ms (77.17%), Space: 31.9 MB (38.58%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
@@ -7,13 +7,12 @@ class Solution:
 
         for num in count:
             length = 0
-            cur_num = num
 
-            while count[cur_num] > 1:
-                cur_num = cur_num * cur_num
+            while count[num] > 1:
+                num = num * num
                 length += 2
             
-            length += 1 if count[cur_num] else -1
+            length += 1 if count[num] else -1
             res = max(res, length)
         
         return res
