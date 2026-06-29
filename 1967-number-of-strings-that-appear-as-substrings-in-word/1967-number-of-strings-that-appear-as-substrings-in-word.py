be032a8661344389ff159e65e5fc008aa78dcc79
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-06-29 08:45:52 -0400

    Time: 0 ms (100%), Space: 19.4 MB (19.78%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,14 +1,9 @@
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        substrings = set()
         res = 0
-
-        for i in range(len(word)):
-            for j in range(i + 1, len(word) + 1):
-                substrings.add(word[i : j])
         
         for pattern in patterns:
-            if pattern in substrings:
+            if pattern in word:
                 res += 1
         
         return res
