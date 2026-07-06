d98b054a61ecb6375d264be2f8ae4b8813146dc0
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-06 10:47:02 -0400

    Time: 5 ms (51.91%), Space: 19.2 MB (88.56%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        mapping = {
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000
+        symbolToValue = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
         }
-
+        
         res = 0
 
         for i in range(len(s)):
-            if (i + 1) < len(s) and mapping[s[i + 1]] > mapping[s[i]]:
-                res -= mapping[s[i]]
+            if i + 1 < len(s) and symbolToValue[s[i]] < symbolToValue[s[i + 1]]:
+                res -= symbolToValue[s[i]]
             else:
-                res += mapping[s[i]]
+                res += symbolToValue[s[i]]
         
         return res
