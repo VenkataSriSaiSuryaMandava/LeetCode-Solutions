ef0cc2076b8b29f205b2dba3f0f6763306e3c7b1
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-06 10:07:37 -0400

    Time: 0 ms (100%), Space: 19.2 MB (73.03%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def arraySign(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
         res = 1
 
-        for n in nums:
-            if n == 0:
-                return 0
-
-            if n < 0:
-                res *= -1
-                
-        return res
+        for num in nums:
+            res *= num
+        
+        if res < 0:
+            return -1
+        elif res > 0:
+            return 1
+        else:
+            return 0
