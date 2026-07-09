a5d18aaa044fdb7573d871b43d07685cd66c1252
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-09 09:36:55 -0400

    Time: 39 ms (45.92%), Space: 19.3 MB (29.5%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,24 +1,27 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if "0" in [num1, num2]:
+        if num1 == '0' or num2 == '0':
             return "0"
-        
-        res = [0] * (len(num1) + len(num2))
+
         num1 = num1[ : : -1]
         num2 = num2[ : : -1]
 
+        res = [0] * (len(num1) + len(num2))
+
         for i in range(len(num1)):
             for j in range(len(num2)):
-                digit = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                digit1 = ord(num1[i]) - ord('0')
+                digit2 = ord(num2[j]) - ord('0')
+                digit = digit1 * digit2
+
                 res[i + j] += digit
                 res[i + j + 1] += res[i + j] // 10
                 res[i + j] = res[i + j] % 10
         
-        res = res[ : : - 1]
         beg = 0
+        res = res[ : : -1]
 
         while beg < len(res) and res[beg] == 0:
             beg += 1
         
-        res = map(str, res[beg : ])
-        return "".join(res)
+        return "".join(map(str, res[beg : ]))
