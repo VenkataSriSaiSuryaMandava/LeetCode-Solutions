250f602a7aa560fefe5a8299a16f0b72606c1a24
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-12 10:43:38 -0400

    Time: 35 ms (76.35%), Space: 37.7 MB (21.83%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,14 +1,8 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        nums = arr.copy()
-        nums.sort()
-
         ranks = {}
-        rank = 1
 
-        for num in nums:
-            if num not in ranks:
-                ranks[num] = rank
-                rank += 1
+        for i, num in enumerate(sorted(set(arr))):
+            ranks[num] = i + 1
 
         return [ranks[num] for num in arr]
