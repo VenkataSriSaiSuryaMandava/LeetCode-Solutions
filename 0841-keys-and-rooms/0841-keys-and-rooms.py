4e017e8d5234f0d237f7fd409931f856b9697a9c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-13 09:48:36 -0400

    Time: 3 ms (24.72%), Space: 19.6 MB (82.31%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -6,7 +6,7 @@ class Solution:
         while stack:
             room = stack.pop()
             visited.add(room)
-            
+
             for key in rooms[room]:
                 if key not in visited:
                     stack.append(key)
