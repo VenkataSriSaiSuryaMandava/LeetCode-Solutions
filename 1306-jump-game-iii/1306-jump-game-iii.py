1082a0a8f2675f2bb395ea9d0d625c4bd328c425
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-15 09:28:33 -0400

    Time: 10 ms (58.96%), Space: 26.5 MB (53.65%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,35 +1,27 @@
-from collections import deque
-
-class Solution(object):
-    def canReach(self, arr, start):
-        """
-        :type arr: List[int]
-        :type start: int
-        :rtype: bool
-        """
-        
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
         n = len(arr)
+        queue = deque([(start)])
         visited = set()
-        q = deque([start])
 
-        while q:
-            i = q.popleft()
+        while queue:
+            i = queue.popleft()
 
             if arr[i] == 0:
                 return True
-
+            
             if i in visited:
                 continue
-
+            
             visited.add(i)
 
             left = i - arr[i]
             right = i + arr[i]
 
             if 0 <= left < n:
-                q.append(left)
-
+                queue.append(left)
+            
             if 0 <= right < n:
-                q.append(right)
-
+                queue.append(right)
+        
         return False
