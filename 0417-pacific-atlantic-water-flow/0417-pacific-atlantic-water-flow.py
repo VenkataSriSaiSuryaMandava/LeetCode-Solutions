3f51223099bf294c5785746a3f6679c848f4d1de
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-16 10:12:50 -0400

    Time: 39 ms (26.12%), Space: 22.2 MB (38.82%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,41 +1,36 @@
-class Solution(object):
-    def pacificAtlantic(self, heights):
-        """
-        :type heights: List[List[int]]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows = len(heights)
         cols = len(heights[0])
 
-        res = []
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         pacific = set()
         atlantic = set()
 
-        def dfs(r, c, visit, prev):
-            if (r < 0 or c < 0 or 
+        def dfs(r, c, visited, prevHeight):
+            if (r < 0 or c < 0 or
                 r == rows or c == cols or
-                (r, c) in visit
-                or heights[r][c] < prev):
+                (r, c) in visited or 
+                heights[r][c] < prevHeight):
                 return
             
-            visit.add((r, c))
+            visited.add((r, c))
 
             for dr, dc in directions:
                 row = r + dr
                 col = c + dc
-
-                dfs(row, col, visit, heights[r][c])
+                dfs(row, col, visited, heights[r][c])
         
         for r in range(rows):
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, cols - 1, atlantic, heights[r][cols - 1])
+            dfs(r, 0, pacific, -1)
+            dfs(r, cols - 1, atlantic, -1)
         
         for c in range(cols):
-            dfs(0, c, pacific, heights[0][c])
-            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
+            dfs(0, c, pacific, -1)
+            dfs(rows - 1, c, atlantic, -1)
         
+        res = []
+
         for r in range(rows):
             for c in range(cols):
                 if (r, c) in pacific and (r, c) in atlantic:
