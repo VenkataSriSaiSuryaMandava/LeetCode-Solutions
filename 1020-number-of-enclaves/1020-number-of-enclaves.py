2289c734d3fdf801e7270c7c74aec525577fad0b
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-16 09:49:56 -0400

    Time: 72 ms (61.7%), Space: 37.5 MB (28.3%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -3,17 +3,14 @@ class Solution:
         rows = len(grid)
         cols =len(grid[0])
 
-        visited = set()
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def dfs(r, c):
             if (r < 0 or c < 0 or
                 r == rows or c == cols or
-                (r, c) in visited or
                 grid[r][c] == 0):
                 return
             
-            visited.add((r, c))
             grid[r][c] = 0
 
             for dr, dc in directions:
