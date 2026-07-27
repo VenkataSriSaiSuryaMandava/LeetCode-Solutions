16d69f1853bddf51e82bde4b78bf94e7e95f0c18
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-27 16:04:34 -0700

    Time: 0 ms (100%), Space: 21.2 MB (48.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        res = None
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         count = 0
+        res = 0
 
         for num in nums:
             if count == 0:
                 res = num
-            
-            if res == num:
+
+            if num == res:
                 count += 1
             else:
                 count -= 1
