18f96212d45526651d8c7ece39afa24b63e8fc14
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-27 15:54:25 -0700

    Time: 16 ms (27.09%), Space: 24.1 MB (9.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,11 +3,11 @@ class Solution:
         anagrams = defaultdict(list)
 
         for word in strs:
-            indexes = [0] * 26
+            count = [0] * 26
 
             for ch in word:
-                indexes[ord(ch) - ord('a')] += 1
+                count[ord(ch) - ord('a')] += 1
             
-            anagrams[tuple(indexes)].append(word)
+            anagrams[tuple(count)].append(word)
         
         return list(anagrams.values())
