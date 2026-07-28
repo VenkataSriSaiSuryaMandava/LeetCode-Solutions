ba80fe033e5e8162015c5acc9aa97c656888448c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-28 10:40:26 -0700

    Time: 5 ms (62.21%), Space: 22.8 MB (60.26%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = defaultdict(int)
+
         for num in nums:
             count[num] += 1
         
-        minHeap = []
+        heap = []
+
         for num, cnt in count.items():
-            heapq.heappush(minHeap, [-cnt, num])
+            heapq.heappush(heap, [-cnt, num])
         
         res = []
-        while k != len(res):
-            cnt, num = heapq.heappop(minHeap)
+
+        while len(res) < k:
+            cnt, num = heapq.heappop(heap)
             res.append(num)
         
         return res
