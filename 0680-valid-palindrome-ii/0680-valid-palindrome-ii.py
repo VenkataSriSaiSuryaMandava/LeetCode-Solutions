defbbe229a1dea7c20682f3f33e129bec18c26f0
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-29 11:03:53 -0700

    Time: 59 ms (50.96%), Space: 19.5 MB (42.45%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,27 +1,23 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        def plaindrome(l, r):
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def palindrome(l, r):
             while l < r:
                 if s[l] != s[r]:
                     return False
                 
                 l += 1
                 r -= 1
-            
+
             return True
-        
+
         l = 0
         r = len(s) - 1
 
         while l < r:
             if s[l] != s[r]:
-                return plaindrome(l + 1, r) or plaindrome(l, r - 1)
+                return palindrome(l, r - 1) or palindrome(l + 1, r)
             
             l += 1
             r -= 1
-        
+
         return True
