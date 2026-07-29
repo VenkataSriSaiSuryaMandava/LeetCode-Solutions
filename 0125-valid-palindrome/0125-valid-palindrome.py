f9f10dea512f04c013369e13bff53df435965ee3
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-29 10:56:14 -0700

    Time: 30 ms (5.69%), Space: 19.5 MB (76.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         def isAlphaNum(ch):
-            return (ord("A") <= ord(ch) <= ord("Z") or
-                    ord("a") <= ord(ch) <= ord("z") or
-                    ord("0") <= ord(ch) <= ord("9"))
+            return (ord('0') <= ord(ch) <= ord('9') or
+                    ord('a') <= ord(ch) <= ord('z') or
+                    ord('A') <= ord(ch) <= ord('Z'))
         
+        def toLower(ch):
+            if ord('A') <= ord(ch) <= ord('Z'):
+                return chr(ord('a') + (ord(ch) - ord('A')))
+            else:
+                return ch
+
         l = 0
         r = len(s) - 1
 
@@ -19,7 +21,7 @@ class Solution(object):
             while l < r and not isAlphaNum(s[r]):
                 r -= 1
             
-            if s[l].lower() != s[r].lower():
+            if toLower(s[l]) != toLower(s[r]):
                 return False
             
             l += 1
