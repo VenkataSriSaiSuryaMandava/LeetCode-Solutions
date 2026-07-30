729018c1971d49837c977c83a5f387c6433db2b9
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-07-30 11:53:22 -0700

    Time: 46 ms (60.88%), Space: 28.6 MB (74.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         res = 0
         l = 0
 
         for r in range(1, len(prices)):
-            if prices[l] >= prices[r]:
+            if prices[l] < prices[r]:
+                res = max(res, prices[r] - prices[l])
+            else:
                 l = r
-            res = max(res, prices[r] - prices[l])
         
         return res
