d3c3fb720ad5a8313a6b17c1dc83b32fe4db9491
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-04 10:37:05 -0700

    Time: 171 ms (87.57%), Space: 35.4 MB (31.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,7 +4,6 @@ class Solution:
         res = []
 
         l = 0
-
         for r in range(len(nums)):
             while queue and nums[queue[-1]] < nums[r]:
                 queue.pop()
@@ -17,5 +16,5 @@ class Solution:
                 
                 res.append(nums[queue[0]])
                 l += 1
-            
+        
         return res
