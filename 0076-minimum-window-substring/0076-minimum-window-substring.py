d01e6c4e1e43e6e8ae7d74f9858fa7211ff1b76b
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-04 10:09:43 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,48 +1,40 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s) or t == "":
             return ""
-        
+
         countT = defaultdict(int)
+        countS = defaultdict(int)
+        
         for ch in t:
             countT[ch] += 1
         
         need = len(countT)
+        have = 0
 
-        window = defaultdict(int)
-        have = 0 
         res = [0, 0]
-        reslen = float("inf")
+        resLen = float("inf")
 
         l = 0
-        for r in range(len(s)):
-            c = s[r]
-            window[c] += 1
 
+        for r, ch in enumerate(s):
+            countS[ch] += 1
 
-            if c in countT and countT[c] == window[c]:
+            if ch in countT and countT[ch] == countS[ch]:
                 have += 1
             
             while have == need:
-                if r - l + 1 < reslen:
-                    reslen = r - l + 1
+                if r - l + 1 < resLen:
+                    resLen = r - l + 1
                     res = [l, r]
-
-                ch = s[l]
-                window[ch] -= 1
                 
-                if ch in countT and countT[ch] > window[ch]:
+                countS[s[l]] -= 1
+                
+                if s[l] in countT and countS[s[l]] < countT[s[l]]:
                     have -= 1
+                
                 l += 1
         
         l, r = res
 
-        if reslen == float("inf"):
-            return ""
-        else:
-            return s[l : r + 1]
+        return s[l : r + 1] if res != float("inf") else ""
