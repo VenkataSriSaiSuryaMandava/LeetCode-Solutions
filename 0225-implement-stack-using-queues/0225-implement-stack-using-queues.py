08c100f69de49605e90160d93634d6f041d4c479
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-04 11:19:20 -0700

    Time: 0 ms (100%), Space: 19.6 MB (16.83%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -6,15 +6,15 @@ class MyStack:
     def push(self, x: int) -> None:
         self.stack.append(x)
 
-    def pop(self) -> int:
         for i in range(len(self.stack) - 1):
-            self.push(self.stack.popleft())
-
+            self.stack.append(self.stack.popleft())
+        
+    def pop(self) -> int:
         return self.stack.popleft()
 
     def top(self) -> int:
-        return self.stack[-1]
-        
+        return self.stack[0]
+
     def empty(self) -> bool:
         return len(self.stack) == 0
 
