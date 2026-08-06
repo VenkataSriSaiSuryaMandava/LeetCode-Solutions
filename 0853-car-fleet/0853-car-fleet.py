f46bc3d8b796f416b781130c6054e467750f468c
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-06 09:44:44 -0700

    Time: 296 ms (7.62%), Space: 40.6 MB (81.75%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def carFleet(self, target, position, speed):
-        """
-        :type target: int
-        :type position: List[int]
-        :type speed: List[int]
-        :rtype: int
-        """
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pairs = [[p, s] for p, s in zip(position, speed)]
-        sortedpairs = sorted(pairs)[ : : -1]
+        pairs = sorted(pairs)[ : : -1]
         stack = []
 
-        for p, s in sortedpairs:
-            stack.append(float(target - p) / s)
+        for p, s in pairs:
+            stack.append((target - p) / s)
 
             if len(stack) > 1 and stack[-1] <= stack[-2]:
                 stack.pop()
