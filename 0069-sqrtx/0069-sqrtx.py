8f2f7d922dd16030591b84d7cd2b8950092d5fae
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-07 09:33:01 -0700

    Time: 3 ms (66.59%), Space: 19.4 MB (6.13%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,10 +1,10 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l = 1
+        l = 0
         r = x
 
         while l <= r:
-            m = (l + r) // 2
+            m = l + (r - l) // 2
 
             if m * m > x:
                 r = m - 1
