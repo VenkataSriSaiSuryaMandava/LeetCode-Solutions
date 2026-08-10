11b56359d3c0add2685efc7b1419c0c2769db223
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-10 12:32:18 -0700

    Time: 0 ms (100%), Space: 19.5 MB (41.86%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A = nums1
-        B = nums2
-
-        total = len(A) + len(B)
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        total = len(nums1) + len(nums2)
         half = total // 2
 
-        if len(B) < len(A):
-            A, B = B, A
-        
-        l = 0
-        r = len(A) - 1
+        l = 0 
+        r = len(nums1) - 1
 
         while True:
             i = (l + r) // 2
             j = half - i - 2
 
-            Aleft = A[i] if i >= 0 else float("-inf")
-            Aright = A[i + 1] if i + 1 < len(A) else float("inf")
-            Bleft = B[j] if j >= 0 else float("-inf")
-            Bright = B[j + 1] if j + 1 < len(B) else float("inf")
+            nums1Left = nums1[i] if i >= 0 else float("-inf")
+            nums1Right = nums1[i + 1] if  i + 1 < len(nums1) else float("inf")
+            nums2Left = nums2[j] if j >= 0 else float("-inf")
+            nums2Right = nums2[j + 1] if j + 1 < len(nums2) else float("inf")
 
-            if Aleft <= Bright and Bleft <= Aright:
+            if nums1Left <= nums2Right and nums2Left <= nums1Right:
                 if total % 2:
-                    return min(Aright, Bright)
+                    return min(nums1Right, nums2Right)
                 else:
-                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                    return (max(nums1Left, nums2Left) + min(nums1Right, nums2Right)) / 2
             
-            if Aleft > Bright:
+            if nums1Left > nums2Right:
                 r = i - 1
             else:
                 l = i + 1
