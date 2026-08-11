2af601d10a0faa1801cbbfde5a613a1399777451
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-11 09:49:23 -0700

    Time: 23 ms (77.35%), Space: 33.5 MB (49.87%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
         slow = nums[0]
         fast = nums[0]
 
