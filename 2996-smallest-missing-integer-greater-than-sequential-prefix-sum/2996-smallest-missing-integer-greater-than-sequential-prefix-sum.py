330f58ea7909c2a4f2e179c7430998485a4fda07
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-11 08:46:32 -0700

    Time: 0 ms (100%), Space: 19.3 MB (51.82%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingInteger(self, nums: List[int]) -> int:
         numsSet = set(nums)
-
         prefix = nums[0]
         
         for i in range(1, len(nums)):
@@ -11,6 +10,6 @@ class Solution:
                 break
         
         while prefix in numsSet:
-                    prefix += 1
+            prefix += 1
                 
         return prefix
