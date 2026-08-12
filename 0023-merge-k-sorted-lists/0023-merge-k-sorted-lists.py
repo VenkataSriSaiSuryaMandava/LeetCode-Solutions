f4782d3fe8cd50adb84eb04bc3255ff488fa4135
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-12 09:42:27 -0700

    Time: 14 ms (26.77%), Space: 22.6 MB (92.97%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,33 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
         
         while len(lists) > 1:
-            mergedlists = []
+            mergedLists = []
 
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if i + 1 < len(lists) else None
 
-                mergedlists.append(self.merge(l1, l2))
+                mergedLists.append(self.merge(l1, l2))
             
-            lists = mergedlists
+            lists = mergedLists
         
         return lists[0]
     
     def merge(self, l1, l2):
         dummy = ListNode()
-        cur = dummy
+        cur = dummy 
 
         while l1 and l2:
             if l1.val < l2.val:
@@ -41,6 +37,7 @@ class Solution(object):
         
         if l1:
             cur.next = l1
+        
         if l2:
             cur.next = l2
         
