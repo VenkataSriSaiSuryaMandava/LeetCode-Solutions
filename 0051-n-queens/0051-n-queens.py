0471514a2699e739ae362c4a203d8e0ce47f97fb
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-24 10:48:21 -0700

    Time: 12 ms (38.76%), Space: 19.7 MB (70.41%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def solveNQueens(self, n):
-        """
-        :type n: int
-        :rtype: List[List[str]]
-        """
-        board = [["."] * n for i in range(n)]
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
+        board = [['.' for j in range(n)] for i in range(n)]
 
         cols = set()
         posDiag = set()
@@ -15,24 +11,23 @@ class Solution(object):
             if r == n:
                 copy = ["".join(row) for row in board]
                 res.append(copy)
-                return 
+                return
             
             for c in range(n):
-                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
+                if c in cols or r + c in posDiag or r - c in negDiag:
                     continue
                 
                 cols.add(c)
                 posDiag.add(r + c)
                 negDiag.add(r - c)
-                board[r][c] = "Q"
+                board[r][c] = 'Q'
 
                 backtrack(r + 1)
 
                 cols.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
-                board[r][c] = "."
+                board[r][c] = '.'
         
         backtrack(0)
-
         return res
