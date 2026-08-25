bcde6e75c0072343570f55cf8cfcbe1ec7ff81c5
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-25 09:52:45 -0700

    Time: 6904 ms (14.51%), Space: 21.6 MB (30.77%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,4 +1,4 @@
-class Trie:
+class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
@@ -8,54 +8,52 @@ class Trie:
 
         for c in word:
             if c not in cur.children:
-                cur.children[c] = Trie()
+                cur.children[c] = TrieNode()
             
             cur = cur.children[c]
         
         cur.endOfWord = True
 
-class Solution(object):
-    def findWords(self, board, words):
-        """
-        :type board: List[List[str]]
-        :type words: List[str]
-        :rtype: List[str]
-        """
-        root = Trie()
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
         for word in words:
             root.addWord(word)
-        
+
         rows = len(board)
         cols = len(board[0])
-        
+
+        visited = set()
         res = set()
-        visit = set()
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def backtrack(r, c, word, node):
             if (r < 0 or c < 0 or
-                r >= rows or c >= cols or
-                (r, c) in visit or
+                r == rows or c == cols or
+                (r, c) in visited or
                 board[r][c] not in node.children):
-                return 
+                return
             
-            visit.add((r, c))
+            visited.add((r, c))
             word += board[r][c]
             node = node.children[board[r][c]]
 
             if node.endOfWord:
                 res.add(word)
                 node.endOfWord = False
-
-            backtrack(r + 1, c, word, node)
-            backtrack(r - 1, c, word, node)
-            backtrack(r, c + 1, word, node)
-            backtrack(r, c - 1, word, node)
             
-            visit.remove((r, c))
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
+
+                backtrack(row, col, word, node)
+            
+            visited.remove((r, c))
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] in root.children:
-                    backtrack(r, c, "", root)
+                backtrack(r, c, "", root)
         
         return list(res)
