20ba73276e94f7af2117918654c6c19b1bd015a6
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-08-27 10:54:46 -0700

    Time: 7 ms (54.62%), Space: 25.3 MB (12.52%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,33 +1,26 @@
-class Solution(object):
-    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :type queries: List[List[int]]
-        :rtype: List[bool]
-        """
-        adj = defaultdict(list)
-
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        preMap = defaultdict(list)
         for pre, crs in prerequisites:
-            adj[crs].append(pre)
+            preMap[crs].append(pre)
         
         def dfs(crs):
-            if crs not in preMap:
-                preMap[crs] = set()
+            if crs not in prereqMap:
+                prereqMap[crs] = set()
 
-                for pre in adj[crs]:
-                    preMap[crs] |= dfs(pre)
+                for pre in preMap[crs]:
+                    prereqMap[crs] |= dfs(pre)
                 
-                preMap[crs].add(crs)
-
-            return preMap[crs]
-
-        preMap = {}
+                prereqMap[crs].add(crs)
+            
+            return prereqMap[crs]
+        
+        prereqMap = {}
         for crs in range(numCourses):
             dfs(crs)
-
+        
         res = []
         for pre, crs in queries:
-            res.append(pre in preMap[crs])
+            res.append(pre in prereqMap[crs])
         
         return res
