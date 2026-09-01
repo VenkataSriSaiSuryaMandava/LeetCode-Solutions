6e31fd183a382713bd962decfcd31765993f9237
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-09-01 11:28:58 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        robleft = self.helper(nums[ : -1])
-        robright = self.helper(nums[1 : ])
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def robHouses(l, r):
+            rob1 = 0
+            rob2 = 0
 
-        return max(nums[0], robleft, robright)
-    
-    def helper(self, nums):
-        rob1 = 0
-        rob2 = 0
+            for i in range(l, r + 1):
+                temp = rob1
+                rob1 = rob2
+                rob2 = max(rob2, temp + nums[i])
 
-        for n in nums:
-            temp = max(rob2, n + rob1)
-            rob1 = rob2
-            rob2 = temp
+            return rob2
         
-        return rob2
+        left = robHouses(0, len(nums) - 2)
+        right = robHouses(1, len(nums) - 1)
+
+        return max(left, right)
