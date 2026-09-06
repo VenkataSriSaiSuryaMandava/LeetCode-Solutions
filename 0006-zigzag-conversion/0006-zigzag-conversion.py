a861ed24d8d80da41e195a32e9b040be61ed0899
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-09-06 15:52:26 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,17 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows <= 1:    return s
-        res = ['' for _ in range(numRows)]
-        direction = True
+        if not s or numRows == 1:   return s
         i = 0
+        res = [""] * numRows
+        direction = True
         for ch in s:
             res[i] += ch
-            if i == numRows-1:
-                direction = False
-            elif i == 0:
+            if i == 0:
                 direction = True
+            elif i == numRows-1:
+                direction = False
             if direction:
                 i += 1
             else:
                 i -= 1
         return ''.join(res)
+
+        
