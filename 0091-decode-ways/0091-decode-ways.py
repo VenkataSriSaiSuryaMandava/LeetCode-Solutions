535822dc856f41856f1f0f2e4478fd812aaf92ce
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-09-14 09:57:26 -0700

    Time: 0 ms (100%), Space: 19.3 MB (40.24%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -5,13 +5,13 @@ class Solution:
         def dfs(i):
             if i in dp:
                 return dp[i]
-
+            
             if s[i] == '0':
                 return 0
             
             dp[i] = dfs(i + 1)
 
-            if (i + 1 < len(s) and 
+            if (i + 1 < len(s) and
                 (s[i] == '1' or
                 s[i] == '2' and s[i + 1] in "0123456")):
                 dp[i] += dfs(i + 2)
