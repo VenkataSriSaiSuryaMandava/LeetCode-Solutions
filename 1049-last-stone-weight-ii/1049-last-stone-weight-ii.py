0c8e77d4eeebc41fa556c96c1c77dedcd13dc608
Venkata Sri Sai Surya Mandava <85121699+VenkataSriSaiSuryaMandava@users.noreply.github.com>
2026-09-17 10:21:34 -0700

    Time: 29 ms (23.39%), Space: 27.1 MB (20.22%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,19 +1,15 @@
-class Solution(object):
-    def lastStoneWeightII(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
+class Solution:
+    def lastStoneWeightII(self, stones: list[int]) -> int:
         stoneSum = sum(stones)
         target = ceil(stoneSum / 2)
         dp = {}
 
         def dfs(i, total):
-            if i == len(stones) or total >= target:
-                return abs(total - (stoneSum - total))
-            
             if (i, total) in dp:
                 return dp[(i, total)]
+            
+            if i == len(stones) or total >= target:
+                return abs(total - (stoneSum - total))
             
             dp[(i, total)] = min(dfs(i + 1, total), dfs(i + 1, total + stones[i]))
 
